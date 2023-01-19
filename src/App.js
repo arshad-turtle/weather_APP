5378c132073fb379283f41cc5d691778ca98bdaf
arshad-turtle <arshad.bagwan@turtlemint.com>
2023-01-19 15:54:54 +0530

    Project Complete

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import Weather from './Components/Weather'
+function App() {
+  return (
+    <>
+    <div className="App" style={{backgroundColor:"yellow", marginTop:"56px" , fontSize:"14px" }} >
+      HELLO WORLD
+
+
+      
+   
+   
+  </div>
+  <div>
+    <Weather/>
+  </div>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Components/InDec.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+function incrementCounte(){
+    let [num,setnum] = useState
+}
